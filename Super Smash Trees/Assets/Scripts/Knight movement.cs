92f78370e7117f2cb705c025b941fc116b878c79
Kevin416A12 <kevin.c.41683@gmail.com>
2025-04-29 00:01:19 -0600

    Ya el personaje se mueve y salta

--- Super Smash Trees/Assets/Scripts/Knight movement.cs
+++ Super Smash Trees/Assets/Scripts/Knight movement.cs
@@ -3,26 +3,58 @@ using UnityEngine;
 public class Knightmovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 155f;
     private Rigidbody2D rb;
-    
-    
+    private Animator anim;
+    private float Horizontal;
+    private bool Grounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     
     // Update is called once per frame
     void Update()
     {
-        // Movimiento   
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveX, moveY);
-        rb.velocity = movement * speed;
-        // Rotación
-        if (movement != Vector2.zero)
+        Horizontal = Input.GetAxis("Horizontal");
+        if (Horizontal < 0) transform.localScale = new Vector3(-3.5f, 3.5f, 1);
+        else if (Horizontal > 0) transform.localScale = new Vector3(3.5f, 3.5f, 1);
+        anim.SetBool("Running", Horizontal != 0); //Si Horizontal es diferente de 0, entonces el personaje está corriendo
+        Debug.DrawRay(transform.position, Vector3.down * 0.7f, Color.red);
+        
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.7f))
         {
-            float angle = Mathf.Atan2(movement.y, movement.x) * Mat
+            Grounded = true;
+        }
+        else Grounded = false;
+        
+        
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump(); 
+        }
+        
     }
+    
+    
+    
+    
+    private void FixedUpdate() //Esto es para que las físicas no dependan de la cantidad de frames a las que vaya el juego
+    {
+       
+        rb.linearVelocity = new Vector2(Horizontal * speed, rb.linearVelocity.y); //rb.velocity es la velocidad del rigidbody
+    }
+    
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce);
+    }
+    
+    
+    
+    
+    
 }
+
